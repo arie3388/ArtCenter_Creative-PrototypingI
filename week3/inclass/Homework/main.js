cf70e751e53e8672797f157a8ca56ce18bde807c
arie3388 <arielle.wiltz@gmail.com>
2023-10-03 19:56:03 -0700

    Interactive Button
    Added two images
    Added Large Image... it broke for some reason
    added javascript

--- week3/inclass/Homework/main.js
+++ week3/inclass/Homework/main.js
@@ -1,4 +1,11 @@
-let btnAddText = document.getElementById("btnAddText")
-let addText = function(){
-    let addText= "Hello World"
+let btn1 = document.getElementById("misty1")
+
+let switchImage = function(imgID){
+    document.getElementById("largeImg").src=this.id+".jpg"
 }
+
+btn1.addEventListener("click", switchImage, "misty1")
+document.getElementById("misty1").addEventListener("click", switchImage("misty1"))
+document.getElementById("misty2").addEventListener("click", switchImage("misty2"))
+document.getElementById("misty3").addEventListener("click", switchImage("misty3"))
+document.getElementById("misty4").addEventListener("click", switchImage("misty4"))
